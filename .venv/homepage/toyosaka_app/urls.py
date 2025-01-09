a0b8d26439aa56d9e4585d58e679d8a973c8d0e0
t-shimizu <fko2347091@stu.o-hara.ac.jp>
2025-01-09 18:23:38 +0900

    add url

--- .venv/homepage/toyosaka_app/urls.py
+++ .venv/homepage/toyosaka_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('contact/',views.ContactView.as_view(),name='contact'),
     path('Achievements/',views.AchieveView.as_view(),name='Achieve'),
     path('Business_content/',views.ContentView.as_view(),name='Business_content'),
-    path('Company_info/',views.info_View.as_view(),name='Company_info'),
+    path('Company_info/',views.info_View.as_view(),name='info'),
     path('Recruitment/',views.Recruitment.as_view(),name='Recruitment'),
 
 ]
