abf2f79f603b58205ffaa2e74c25f9ef1f2833fb
t-shimizu <fko2347091@stu.o-hara.ac.jp>
2025-02-27 20:27:09 +0900

    fix detail

--- .venv/homepage/toyosaka_app/urls.py
+++ .venv/homepage/toyosaka_app/urls.py
@@ -10,10 +10,10 @@ app_name='toyosaka_app'
 urlpatterns=[
     path('',views.IndexView.as_view(),name='index'),
     path('contact/',views.ContactView.as_view(),name='contact'),
-    path('Achievements/',views.AchieveView.as_view(),name='Achieve'),
+    path('Achievements/',views.AchieveView.as_view(),name='achievements'),
     path('Business_content/',views.ContentView.as_view(),name='Business_content'),
     path('Company_info/',views.info_View.as_view(),name='info'),
     path('Recruitment/',views.Recruitment.as_view(),name='Recruitment'),
-    path('service/<int:pk>/',views.ServiceDetailView.as_view(), name='service_detail'),
+    path('service/<int:pk>/',views.ServiceDetailView.as_view(),name='service_detail'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
