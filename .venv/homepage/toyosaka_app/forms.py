141f6e045dfad96bd963f70c256411f76fc6d366
t-shimizu <fko2347091@stu.o-hara.ac.jp>
2025-02-28 16:10:13 +0900

    fix contact

--- .venv/homepage/toyosaka_app/forms.py
+++ .venv/homepage/toyosaka_app/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+import re
+from django.core.exceptions import ValidationError
+
+
 
 class ContactForm(forms.Form):
     name=forms.CharField(label='お名前')
@@ -17,3 +21,11 @@ class ContactForm(forms.Form):
         self.fields['title'].widget.attrs['class']='form-control'
         self.fields['message'].widget.attrs['placeholder']='本文を入力してください'
         self.fields['message'].widget.attrs['class']='form-control'
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+        if not re.match(email_regex, email):
+            raise ValidationError('無効なメールアドレスです。有効な形式で入力してください。')
+
+        return email
